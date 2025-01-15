d52a13cdf5dce32ceb7b15c847f3ec2b8d1fb22b
Dogukanerzurum06 <93072322+Dogukanerzurum06@users.noreply.github.com>
2025-01-15 15:41:14 +0300

    footer düzenlendi

--- app/_components/Footer.jsx
+++ app/_components/Footer.jsx
@@ -10,35 +10,8 @@ function Footer() {
     </div>
 
     <p className="mx-auto mt-6 max-w-md text-center leading-relaxed text-gray-500">
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt consequuntur amet culpa cum
-      itaque neque.
+      Bu site Cem Yılmaz'ın Gora filminden esinlenilerek Doğukan Erzurum ve Ömer Polat tarafından geliştirilmiştir.
     </p>
-
-    <ul className="mt-12 flex flex-wrap justify-center gap-6 md:gap-8 lg:gap-12">
-      <li>
-        <a className="text-gray-700 transition hover:text-gray-700/75" href="#"> About </a>
-      </li>
-
-      <li>
-        <a className="text-gray-700 transition hover:text-gray-700/75" href="#"> Careers </a>
-      </li>
-
-      <li>
-        <a className="text-gray-700 transition hover:text-gray-700/75" href="#"> History </a>
-      </li>
-
-      <li>
-        <a className="text-gray-700 transition hover:text-gray-700/75" href="#"> Services </a>
-      </li>
-
-      <li>
-        <a className="text-gray-700 transition hover:text-gray-700/75" href="#"> Projects </a>
-      </li>
-
-      <li>
-        <a className="text-gray-700 transition hover:text-gray-700/75" href="#"> Blog </a>
-      </li>
-    </ul>
 
     <ul className="mt-12 flex justify-center gap-6 md:gap-8">
       <li>
